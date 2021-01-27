35d42746b4be2f8a67f8df2353f6c6722fb831d6
Oniris <72936212+Oniris@users.noreply.github.com>
2021-01-27 10:11:05 +0100

    2.4 et 2.5 pas fini

---
+++ Crypto_MAUBLANC_BOCLETRICHTER/src/crypto_maublanc_bocletrichter/algorithmes/chiffrement_algorithme/Algorithme.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crypto_maublanc_bocletrichter.algorithmes.chiffrement_algorithme;
+
+import crypto_maublanc_bocletrichter.donnees.cles.Cles;
+import crypto_maublanc_bocletrichter.donnees.messages.Message;
+
+/**
+ *
+ * @author iris et albane
+ */
+public class Algorithme {
+    public String nom; //Attribut en lecture seule
+
+    /**
+     * Getter de l'attribut nom
+     * @return String
+     */
+    public String getNom() {
+        return nom;
+    }
+    
+    /**
+     * Chiffre le message donné en paramètre à l'aide éventuellement d'un ensemble de clés publiques et d'un ensemmble de clés privées
+     * @param message Message
+     * @param clesPubliques Cles
+     * @param clesPrivees Cles
+     * @return Message
+     */
+    public Message chiffrer(Message message, Cles clesPubliques, Cles clesPrivees){
+        Message m = null;
+        return m;
+    }
+    
+    /**
+     * Déchiffre le message donné en paramètre à l'aide éventuellement d'un ensemble de clés publiques et d'un ensemmble de clés privées
+     * @param message Message
+     * @param clesPubliques Cles
+     * @param clesPrivees Cles
+     * @return Message
+     */
+    public Message dechiffrer(Message message, Cles clesPubliques, Cles clesPrivees){
+        Message m = null;
+        return m;
+    }
+}
---
+++ Crypto_MAUBLANC_BOCLETRICHTER/src/crypto_maublanc_bocletrichter/algorithmes/generateurcles/GenerateurDeCles.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crypto_maublanc_bocletrichter.algorithmes.generateurcles;
+
+import crypto_maublanc_bocletrichter.donnees.cles.Cles;
+
+/**
+ *
+ * @author iris et albane
+ */
+public class GenerateurDeCles {
+    /**
+     * Permet de générer une clé publique
+     * @return Cles
+     */
+    public Cles genererClePublique(){
+        return null;
+    }
+    
+    /**
+     * Permet de générer une clé privée
+     * @return Cles
+     */
+    public Cles genererClePrivee(){
+        return null;
+    }
+}
