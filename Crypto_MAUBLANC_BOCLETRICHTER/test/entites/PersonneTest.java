ac763f94753181a062aadb346b4f4a1d5b7f542a
Oniris <72936212+Oniris@users.noreply.github.com>
2021-01-27 11:15:25 +0100

    Update PersonneTest.java

--- Crypto_MAUBLANC_BOCLETRICHTER/test/entites/PersonneTest.java
+++ Crypto_MAUBLANC_BOCLETRICHTER/test/entites/PersonneTest.java
@@ -5,6 +5,11 @@
  */
 package entites;
 
+import crypto_maublanc_bocletrichter.algorithmes.chiffrement_algorithme.Algorithme;
+import crypto_maublanc_bocletrichter.donnees.cles.Cles;
+import crypto_maublanc_bocletrichter.donnees.messages.Message;
+import crypto_maublanc_bocletrichter.entites.Personne;
+import java.lang.reflect.Field;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,32 +19,125 @@ import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
- * @author Utilisateur
+ * @author Matthieu
  */
 public class PersonneTest {
     
-    public PersonneTest() {
+    @Test
+    public void testStructure() {
+        System.out.println("Test de structure");
+        Personne instance = new Personne("Alice");
+        Field fields[] = Personne.class.getDeclaredFields();
+        assertEquals(3,fields.length);
+        
+        boolean isNomHere = false;
+        boolean isAlgorithmeHere = false;      
+        boolean isClesPriveesHere = false;
+        
+        for(Field field : fields) {
+            field.setAccessible(true);
+            switch(field.getName()) {
+                case "nom" : isNomHere = true; break;
+                case "algorithme" : isAlgorithmeHere = true; break;
+                case "clesPrivees" : isClesPriveesHere = true; break;
+            }
+        }
+        assertEquals(true, isNomHere,"Champs nom absent");
+        assertEquals(true, isAlgorithmeHere,"Champs algorithme absent");
+        assertEquals(true, isClesPriveesHere,"Champs clesPrivees absent");
     }
     
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
+    @Test
+    public void testGetNom() {
+        System.out.println("Test de la méthode getNom");
+        Personne instance = new Personne("Alice");
+        String expResult = "Alice";
+        String result = instance.getNom();
+        assertEquals(expResult, result);
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    /**
+     * Test of setAlgorithme method, of class Personne.
+     */
+    @Test
+    public void testSetAlgorithme() throws IllegalArgumentException, IllegalAccessException {
+        System.out.println("Test de la méthode setAlgorithme");
+        Algorithme algorithme = new AlgorithmeFake();
+        Personne instance = new Personne("Alice");
+        instance.setAlgorithme(algorithme);
+        
+        Field fields[] = Personne.class.getDeclaredFields();
+        for(Field field : fields) {
+            field.setAccessible(true);
+            if(field.getName().equals("algorithme")) {
+                assertEquals(algorithme,(Algorithme) field.get(instance));
+            }
+        }
+    }
+
+    /**
+     * Test of getAlgorithme method, of class Personne.
+     */
+    @Test
+    public void testGetAlgorithme() {
+        System.out.println("Test de la méthode getAlgorithme");
+        Algorithme algorithme = new AlgorithmeFake();
+        Personne instance = new Personne("Alice");
+        instance.setAlgorithme(algorithme);
+        assertEquals(algorithme,instance.getAlgorithme());
+    }
+
+    /**
+     * Test of setClesPrivees method, of class Personne.
+     */
+    @Test
+    public void testSetClesPrivees() throws IllegalArgumentException, IllegalAccessException {
+        System.out.println("Test de la méthode setClesPrivees");
+        Cles cles = new Cles();
+        Personne instance = new Personne("Alice");
+        instance.setClesPrivees(cles);
+        
+        Field fields[] = Personne.class.getDeclaredFields();
+        for(Field field : fields) {
+            field.setAccessible(true);
+            if(field.getName().equals("clesPrivees")) {
+                assertEquals(cles,(Cles) field.get(instance));
+            }
+        }
+    }
+
+    /**
+     * Test of chiffrer method, of class Personne.
+     */
+    @Test
+    public void testChiffrer() throws Exception {
+        System.out.println("Test de la méthode Chiffrer");
+        Algorithme algorithme = new AlgorithmeFake();
+        Personne instance = new Personne("Alice");
+        instance.setAlgorithme(algorithme);
+        
+        Message message = null;
+        Cles clesPubliques = null;
+        String expResult = "Chiffre";
+        Message result = instance.chiffrer(message, clesPubliques);
+        assertEquals(expResult, result.asString());
+    }
+
+    /**
+     * Test of dechiffrer method, of class Personne.
+     */
+    @Test
+    public void testDechiffrer() throws Exception {
+        System.out.println("Test de la méthode Dechiffrer");
+        Algorithme algorithme = new AlgorithmeFake();
+        Personne instance = new Personne("Alice");
+        instance.setAlgorithme(algorithme);
+        
+        Message message = null;
+        Cles clesPubliques = null;
+        String expResult = "Dechiffre";
+        Message result = instance.dechiffrer(message, clesPubliques);
+        assertEquals(expResult, result.asString());
+    }
+    
 }
