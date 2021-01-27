6c5a9abdf2bff56d34739d308bfe89f008f8bf4e
Oniris <72936212+Oniris@users.noreply.github.com>
2021-01-27 11:56:21 +0100

    Update Univers.java

--- Crypto_MAUBLANC_BOCLETRICHTER/src/crypto_maublanc_bocletrichter/entites/Univers.java
+++ Crypto_MAUBLANC_BOCLETRICHTER/src/crypto_maublanc_bocletrichter/entites/Univers.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
  * @author iris et albane
  */
 public class Univers {
-    private Univers instance;
+    private static Univers instance = new Univers();
     private HashMap<String, Cles> listeClesPubliques;
     private HashMap<String, Message> listMessagesPublics;
     
@@ -22,26 +22,51 @@ public class Univers {
      * Constructeur de la classe Univers
      */
     private Univers(){
-        
+        this.listMessagesPublics = new HashMap<String,Message>();
+        this.listeClesPubliques = new HashMap<String,Cles>();
     }
     
+    /**
+     * Getter de l'attribut instance
+     * @return Univers
+     */
     public Univers get(){
-        
+        return this.instance;
     }
     
+    /**
+     * Permet d'ajouter une clé à la liste listeClesPubliques
+     * @param identifiant String
+     * @param cles Cles
+     */
     public void addCles(String identifiant, Cles cles){
-        
+        this.instance.listeClesPubliques.put(identifiant, cles);
     }
     
+    /**
+     * Getter d'une clé l'attribut listeClesPubliques
+     * @param identifiant
+     * @return 
+     */
     public Cles getCles(String identifiant){
-        
+        return this.instance.listeClesPubliques.get(identifiant);
     }
     
+    /**
+     * Permet d'ajouter un message à la liste listMessagesPublics
+     * @param identifiant String
+     * @param message Message
+     */
     public void addMessage(String identifiant, Message message){
-        
+        this.instance.listMessagesPublics.put(identifiant, message);
     }
     
+    /**
+     * Getter d'un message de l'attribut listMessagesPublics
+     * @param identifiant
+     * @return Message
+     */
     public Message getMessage(String identifiant){
-        
+        return this.instance.listMessagesPublics.get(identifiant);
     }
 }
