78ef00e9d940bb7a5cf6e37a84d05b2fc68f8011
albane-maublanc <albane-m@live.fr>
2021-01-27 12:06:40 +0100

    Réorganisation tests
    j'ai tout mis

---
+++ Crypto_MAUBLANC_BOCLETRICHTER/test/algorithmes/chiffrement/AlgorithmeFake.java
@@ -0,0 +1,35 @@
+package algorithmes.chiffrement;
+
+import crypto_maublanc_bocletrichter.algorithmes.chiffrement_algorithme.Algorithme;
+import crypto_maublanc_bocletrichter.donnees.cles.Cles;
+import crypto_maublanc_bocletrichter.donnees.messages.Message;
+import crypto_maublanc_bocletrichter.donnees.messages.MessageString;
+import crypto_maublanc_bocletrichter.exceptions.ExceptionCryptographie;
+import donnees.cles.Cles;
+import donnees.messages.Message;
+import donnees.messages.MessageString;
+import exceptions.ExceptionCryptographie;
+
+
+/**
+ * Description de la classe
+ * @author Matthieu
+ */
+public class AlgorithmeFake implements Algorithme {
+
+    @Override
+    public String getNom() {
+       return "Algorithme de Test";
+    }
+
+    @Override
+    public Message chiffrer(Message message, Cles clesPubliques, Cles clesPrivees){
+        return new MessageString("Chiffre");
+    }
+
+    @Override
+    public Message dechiffrer(Message message, Cles clesPubliques, Cles clesPrivees){
+        return new MessageString("Dechiffre");
+    }
+
+}
